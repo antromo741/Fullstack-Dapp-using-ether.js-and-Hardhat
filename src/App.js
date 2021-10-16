e7b5576aa262dd410086d106a5b7a390505c19f8
antromo741 <anthonyroma741@gmail.com>
2021-10-16 12:55:00 -0400

    added the buttons to connect the walalet and send the greeting

--- src/App.js
+++ src/App.js
@@ -44,22 +44,13 @@ function App() {
     }
   }
 
-  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
       </header>
     </div>
   );
