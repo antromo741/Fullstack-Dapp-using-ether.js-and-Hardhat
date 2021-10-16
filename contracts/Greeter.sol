94686554786eefc04461c24427464b3f34402759
Anthony Romo <anthony@mekonomi.onmicrosoft.com>
2021-10-16 12:33:57 -0400

    added the imports

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import "hardhat/console.sol";
 
 contract Greeter {
